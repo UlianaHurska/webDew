30670c723986461b6662e1e5e37a2b7740e22b7b
Uliana Hurska <ulyana1gurska@gmail.com>
2022-02-22 16:24:35 +0200

    first commit

---
+++ first.js
@@ -0,0 +1,19 @@
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+    sayHello() {
+        console.log(this.name + "Hello");
+    }
+}
+
+const alex = new User("Alex");
+console.log(alex);
+console.log(typeof User.constructor());
+
+class Person extends User {
+    constructor(name, email) {
+        super(name);
+        this.email = email;
+    }
+}
